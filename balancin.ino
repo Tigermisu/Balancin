326b4ad57aaf99b5efbca43bede261960f7045cd
Unknown <tigermisu@outlook.com>
2017-03-31 09:38:18 -0700

    uploaded prototype .ino

---
+++ balancin.ino
@@ -0,0 +1,269 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 2
+#define LED_PIN 13 
+
+
+MPU6050 mpu;
+bool blinkState = false;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation variables
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+//PID control variables
+
+  float CurrentValue=0;
+  float U=0;
+  float UAnterior=0;
+  float Error=0;
+  float ErrorAnterior=0;
+  float ErrorPreAnterior=0;
+  float Kp=7.07;
+  float Ki=0.19;
+  float Kd=0.01;         
+  float Referencia=(-5);
+  float PWMdouble;
+  int PWM;
+
+
+//motor
+
+int ena1=3;
+int ena2=4;
+
+int in1=31;
+int in2=33;
+int in3=35;
+int in4=37;
+
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    //h bridge pins for the output
+    pinMode(ena1, OUTPUT);
+    pinMode(ena2, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+
+    // initialize device
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+
+    Serial.println(F("Start"));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+    pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+
+
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            
+            
+          CurrentValue=ypr[2] * 180/M_PI;
+
+      
+      Error=Referencia-CurrentValue;
+      U=Kp*(Error-ErrorAnterior)+Ki*Error+Kd*(Error-2*ErrorAnterior+ErrorPreAnterior)+UAnterior;
+      
+      ErrorPreAnterior=ErrorAnterior;     
+      UAnterior=U;
+      ErrorAnterior=Error;
+
+      
+
+
+
+      if(U<-256)
+      {
+        U=-255;
+        UAnterior=-255;
+      }
+
+      
+      if(U>255)
+      {
+      
+        U=255;
+        UAnterior=255;
+      }
+
+
+      
+          if(CurrentValue>Referencia){
+
+          digitalWrite(in1,LOW);
+          digitalWrite(in2,HIGH);
+
+          digitalWrite(in3,LOW);
+          digitalWrite(in4,HIGH);
+          
+
+          }
+        
+          if(CurrentValue<Referencia){
+        
+          digitalWrite(in1,HIGH);
+          digitalWrite(in2,LOW);
+
+          digitalWrite(in3,HIGH);
+          digitalWrite(in4,LOW);
+        
+          }
+            
+          analogWrite(ena1,abs(U));       
+          analogWrite(ena2, abs(U));
+          
+            
+
+            
+            
+
+            
+            Serial.print("Esfuerzo: ");
+            Serial.println(U);
+            /*
+            Serial.print("CurrentValue: ");
+            Serial.println(CurrentValue);
+            Serial.print("Referencia: ");
+            Serial.println(Referencia);
+            Serial.print("Error: ");
+            Serial.println(Error);
+            Serial.print("UAnterior: ");
+            Serial.println(UAnterior);
+            Serial.print("ErrorAnterior: ");
+            Serial.println(ErrorAnterior);
+            Serial.print("ErrorPreAnterior: ");
+            Serial.println(ErrorPreAnterior);
+            Serial.println(" ");
+            Serial.println(" ");
+            Serial.println(" ");*/
+
+
+        #endif
+
+    }
+}
